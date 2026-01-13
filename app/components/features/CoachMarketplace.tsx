056acb31893f3f991db48e2633fbc17572e067e1
Kayode96-max <olukayodeusman406@gmail.com>
2026-01-13 15:58:58 +0100

    feat: add 'use client' directive to CoachMarketplace and PuzzleTraining for client-side rendering

--- app/components/features/CoachMarketplace.tsx
+++ app/components/features/CoachMarketplace.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useState } from 'react';
 import { useChessCoach } from '../../hooks/useAdvancedContracts';
 import {
--- app/components/features/PuzzleTraining.tsx
+++ app/components/features/PuzzleTraining.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useState, useEffect } from 'react';
 import { useChessPuzzles } from '../../hooks/useAdvancedContracts';
 import PuzzleBoard from '../chess/PuzzleBoard';
