a2789c108b64a905ff0ce6b4762a75997d4dc9c7
Kayode96-max <olukayodeusman406@gmail.com>
2026-01-14 14:12:23 +0100

    feat: integrate UberMoto component into App layout

--- app/new/src/App.tsx
+++ app/new/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import ChessBoard from './ChessBoard';
+import UberMoto from './UberMoto';
 
 function App() {
   return (
     <div className="App">
       <h1>Chess App</h1>
       <ChessBoard />
+      <UberMoto />
     </div>
   );
 }
