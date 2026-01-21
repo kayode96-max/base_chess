968122f3c7dbab4df5caf9f8bc8b57e3d9c15577
Kayode96-max <olukayodeusman406@gmail.com>
2026-01-21 18:58:05 +0100

    feat: add mobile wallet components to wallet index for improved functionality

---
+++ public/New/src/components/wallet/index.ts
@@ -0,0 +1,29 @@
+export { default as ConnectButton } from './ConnectButton';
+export { default as WalletSelector } from './WalletSelector';
+export { default as MobileWalletSelector } from './MobileWalletSelector';
+export { default as QRCodeDisplay } from './QRCodeDisplay';
+export { default as AccountDisplay } from './AccountDisplay';
+export { default as ConnectionStatus } from './ConnectionStatus';
+export { default as TransactionStatus } from './TransactionStatus';
+export { default as WalletErrorBoundary } from './WalletErrorBoundary';
+export { default as WalletProvider } from './WalletProvider';
+export { default as ProviderInitializer } from './ProviderInitializer';
+export { default as EnhancedWalletProvider } from './EnhancedWalletProvider';
+export { default as ConfigurationStatus } from './ConfigurationStatus';
+export { default as AccountSelector } from './AccountSelector';
+export { default as AccountList } from './AccountList';
+export { default as AccountManagementModal } from './AccountManagementModal';
+
+// Mobile wallet components
+export { default as MobileWalletHelp } from './MobileWalletHelp';
+export { default as MobileWalletConnectionStatus } from './MobileWalletConnectionStatus';
+export { default as MobileWalletConnectionHistory } from './MobileWalletConnectionHistory';
+export { default as MobileWalletSettings } from './MobileWalletSettings';
+export { default as MobileWalletTutorial } from './MobileWalletTutorial';
+export { default as MobileWalletFAQ } from './MobileWalletFAQ';
+export { default as MobileWalletConnectionRecovery } from './MobileWalletConnectionRecovery';
+export { default as MobileWalletPerformanceMonitor } from './MobileWalletPerformanceMonitor';
+export { default as MobileWalletConnectionHealthCheck } from './MobileWalletConnectionHealthCheck';
+export { default as MobileWalletSupportHub } from './MobileWalletSupportHub';
+export { default as MobileWalletOnboarding } from './MobileWalletOnboarding';
+export { default as MobileWalletConnectionSummary } from './MobileWalletConnectionSummary';
