fcf16f123e1c7430a62e47edcbb0213fa28bcdfe
Kayode96-max <olukayodeusman406@gmail.com>
2026-01-25 14:44:15 +0100

    chore: remove public/New/src/app/transactions/page.tsx

--- public/New/src/app/transactions/page.tsx
+++
@@ -1,9 +0,0 @@
-import { TransactionHistory } from '@/components/transactions/TransactionHistory';
-
-export default function TransactionsPage() {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <TransactionHistory />
-    </div>
-  );
-}
