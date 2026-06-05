a12eae14423d84b248304327b2d5e4e95dc31638
Kayode96-max <olukayodeusman406@gmail.com>
2026-06-05 08:09:25 +0100

    Add frontend/src/chess/components/Popup/GameEnds/GameEnds.jsx

---
+++ frontend/src/chess/components/Popup/GameEnds/GameEnds.jsx
@@ -0,0 +1,45 @@
+import { Status } from '../../../constants';
+import { useAppContext }from '../../../contexts/Context'
+import { setupNewGame } from '../../../reducer/actions/game';
+import './GameEnds.css'
+
+const GameEnds = ({onClosePopup}) => {
+
+    const { appState : {status, gameMode} , dispatch } = useAppContext();
+    
+    if (status === Status.ongoing || status === Status.promoting)
+        return null
+
+    const isPuzzle = gameMode === 'puzzle';
+
+    const newGame = () => {
+        dispatch(setupNewGame(gameMode)); // Pass gameMode when setting up new game
+    }
+
+    const isWin = status.endsWith('wins')
+    const message = isPuzzle
+        ? status === Status.white
+            ? '🎉 Puzzle Solved!'
+            : status === Status.black
+                ? 'Puzzle Failed'
+                : 'Draw!'
+        : gameMode === 'pvc'
+            ? status === Status.white
+                ? 'You win!'
+                : status === Status.black
+                    ? 'You lost!'
+                    : 'Draw!'
+            : isWin ? status : 'Draw!';
+
+    return <div className="popup--inner popup--inner__center">
+        <h1>{message}</h1>
+        <p>{!isWin && status}</p> {/* Only display original status if it's a draw and not pvc */}
+        <div className={`${status}`}/>
+        <button onClick={newGame}>
+            {isPuzzle ? 'Next Puzzle' : 'New Game'}
+        </button>
+    </div>
+   
+}
+
+export default GameEnds
